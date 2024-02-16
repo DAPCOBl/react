777c8b2415698a4f69bf79c32999e2778526ac74
DAPCOBl <granadosxdapcox@gmail.com>
2024-02-16 11:44:42 -0500

    Actualizar despues de DELETE

--- src/components/cardRepuesto.jsx
+++ src/components/cardRepuesto.jsx
@@ -4,29 +4,31 @@ function RepuestosList() {
   const [repuestos, setRepuestos] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchRepuestos = async () => {
-      try {
-        const response = await fetch('../api/repuesto');
-        if (!response.ok) {
-          throw new Error('Error al obtener los datos de los repuestos');
-        }
-        const repuestosData = await response.json();
-        setRepuestos(repuestosData);
-      } catch (error) {
-        setError(error.message);
+  const fetchRepuestos = async () => {
+    try {
+      const response = await fetch('../api/repuesto');
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos de los repuestos');
       }
-    };
+      const repuestosData = await response.json();
+      setRepuestos(repuestosData);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
+  useEffect(() => {
     fetchRepuestos();
   }, []);
-
 
   const deleteRepuesto = async (_id) => {
     try {
       const response = await fetch(`../api/repuesto/${_id}`, {
         method: 'DELETE'
       });
+      if (!response.ok) {
+        throw new Error('Error al eliminar el repuesto');
+      }
       fetchRepuestos();
     } catch (error) {
       setError(error.message);
@@ -36,14 +38,14 @@ function RepuestosList() {
   return (
     <div>
       <h1>Lista de Repuestos</h1>
-      {error && <p>Error: {error}</p>}
+      {error && <p>{error}</p>}
       <ul>
         {repuestos.map(repuesto => (
           <li key={repuesto._id}>
-            <img src={repuesto.urlImg}/>
+            <img src={repuesto.urlImg} alt={repuesto.nombre}/>
             <h2>{repuesto.nombre}</h2>
             <p>Precio: ${repuesto.precio}</p>
-            <p>ID: ${repuesto._id}</p>
+            <p>ID: {repuesto._id}</p>
             <p>Descripción: {repuesto.descripcionRepuesto}</p>
             <p>Referencia: {repuesto.referencia}</p>
             <p>Modelo: {repuesto.modelo}</p>
