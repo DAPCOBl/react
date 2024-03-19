ba97406a6ae25f05d25d910c7a588bf077e5f9e2
DAPCOBl <granadosxdapcox@gmail.com>
2024-03-19 18:53:06 -0500

    detailsRepuesto

--- src/app/api/repuesto/[_id]/route.js
+++ src/app/api/repuesto/[_id]/route.js
@@ -28,53 +28,43 @@ export async function DELETE(request, { params }) {
   }
 }
 
-exports.getRepuesto = async (req, res) => {
-  try {
-    const repuesto = await Repuesto.findById(req.params.id);
-    res.json(repuesto);
-  } catch (error) {
-    res.status(500).json({ message: 'Error al obtener la repuesto' });
-  }
-};
-
-
 export async function GET(request, { params }) {
   try {
-    const result = await Repuesto.findById(params._id);
-
-    if (!result) {
-      return new Response(
-        JSON.stringify({
-          message: "Repuesto no encontrado",
-        }),
-        {
-          status: 404,
-        }
-      );
-    }
-
-    return new Response(null, {
-      status: 200,
-      body: {
-        _id: result._id,
-        urlImg: result.urlImg,
-        nombre: result.nombre,
-        descripcionRepuesto: result.descripcionRepuesto,
-        referencia: result.referencia,
-        precio: result.precio,
-        modelo: result.modelo,
-        marca: result.marca,
-        tipoRepuesto: result.tipoRepuesto,
-        tipoGarantia: result.tipoGarantia,
-        condicion: result.condicion,
-      },
-    });
+     const result = await Repuesto.findById(params._id);
+ 
+     if (!result) {
+       return new Response(
+         JSON.stringify({
+           message: "Repuesto no encontrado",
+         }),
+         {
+           status: 404,
+         }
+       );
+     }
+ 
+     return new Response(JSON.stringify({
+       _id: result._id,
+       urlImg: result.urlImg,
+       nombre: result.nombre,
+       descripcionRepuesto: result.descripcionRepuesto,
+       referencia: result.referencia,
+       precio: result.precio,
+       modelo: result.modelo,
+       marca: result.marca,
+       tipoRepuesto: result.tipoRepuesto,
+       tipoGarantia: result.tipoGarantia,
+       condicion: result.condicion,
+     }), {
+       status: 200,
+     });
   } catch (error) {
-    return new Response(
-      JSON.stringify({
-        message: error.message,
-      }),
-      { status: 500 }
-    );
+     return new Response(
+       JSON.stringify({
+         message: error.message,
+       }),
+       { status: 500 }
+     );
   }
-}
+ }
+ 
