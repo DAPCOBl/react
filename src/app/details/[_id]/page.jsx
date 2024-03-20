73bf831638a5ffb7bd9a1fc023c8c9592ba1794b
jdmaldonad <josuemadonado56@gmail.com>
2024-03-20 18:32:30 -0500

    details css

--- src/app/details/[_id]/page.jsx
+++ src/app/details/[_id]/page.jsx
@@ -1,13 +1,16 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
+import M from 'materialize-css';
+/**import Loading from '../../../components/loader';**/
 
 export default function Producto() {
   const params = useParams();
-  console.log(params._id);
   const [repuesto, setRepuesto] = useState(null);
+  /**const [loading, setLoading] = useState(true);**/
 
   useEffect(() => {
+    M.AutoInit();
     async function fetchData() {
       try {
         const response = await fetch(`../api/repuesto/${params._id}`);
@@ -24,13 +27,74 @@ export default function Producto() {
     fetchData();
   }, [params._id]);
 
+  function aparecerdesp() {
+    document.getElementById('descripcion').style.display = "block";
+    document.getElementById('especificacion').style.display = "none";
+    document.getElementById('raya1').style.display = "block";
+    document.getElementById('raya2').style.display = "none";
+  }
+
+  function aparecerespe() {
+    document.getElementById('descripcion').style.display = "none";
+    document.getElementById('especificacion').style.display = "block";
+    document.getElementById('raya1').style.display = "none";
+    document.getElementById('raya2').style.display = "block";
+  }
+
+  /**if (loading) {
+    return <Loading loading={loading} />;
+  }**/
+
   return (
     <div>
       <h1>Producto</h1>
       {repuesto && (
         <>
-          <h1>{repuesto.nombre}</h1>
-          <p>{repuesto.descripcionRepuesto}</p>
+          <div className="details-contenedor-producto">
+            <div className="details-producto">
+              <div className="details-imagen">
+                <img className="materialboxed" src={repuesto.urlImg} alt="" id="details-img1" />
+              </div>
+              <div className="info-pro">
+                <h5>{repuesto.nombre}</h5>
+                <h1>{repuesto.marca}</h1>
+                <h2>{repuesto.precio}</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="contenedor-descripcion">
+            <div className="sobre">
+              <h1>SOBRE EL PRODUCTO</h1>
+            </div>
+            <div className="cat">
+              <div className=" cat-descp" onClick={aparecerdesp}>
+                <h3>Caracteristicas</h3>
+                <div className="details-raya1" id="raya1"></div>
+              </div>
+              <div className=" cat-espe" onClick={aparecerespe}>
+                <h3>ESPECIFICACIONES</h3>
+                <div className="details-raya2" id="raya2"></div>
+              </div>
+            </div>
+            <div className="details-descripcion" id="descripcion">
+              <ul>
+                <p>Referencia: {repuesto.referencia}</p>
+                <p>Modelo: {repuesto.modelo}</p>
+                <p>Marca: {repuesto.marca}</p>
+                <p>Tipo de Repuesto: {repuesto.tipoRepuesto}</p>
+                <p>Tipo de Garantía: {repuesto.tipoGarantia}</p>
+                <p>Condición: {repuesto.condicion}</p>
+              </ul>
+            </div>
+            <div className="especificacion" id="especificacion">
+              <div className="contenido1">
+                <div className="parr">
+                  {repuesto.descripcion}
+                </div>
+              </div>
+            </div>
+          </div>
         </>
       )}
     </div>
