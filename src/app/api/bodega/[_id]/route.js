d551c51fabaff8229eec69111b03dc3968704e94
espitianico <nicolas123espitia@gmail.com>
2024-03-25 08:05:52 -0500

    Bodegas

--- src/app/api/bodega/[_id]/route.js
+++ src/app/api/bodega/[_id]/route.js
@@ -1,14 +1,58 @@
-import Bodega from '../../../../models/bodega';
-
-
+// Importación de módulos
+import Bodega from '../../../../models/bodega'; 
+/*
+    Manejador de solicitudes DELETE para eliminar una bodega en la base de datos.
+*/
 export async function DELETE(request, { params }) {
   try {
+// Eliminar un nuevo documento de bodega con el id proporcionado.
     const result = await Bodega.findByIdAndDelete(params._id);
+
+     //  Verificar si la bodega no fue encontrada
+    if (!result) {
+      return new Response(
+        JSON.stringify({
+          message: "Bodega no encontrada",
+        }),
+        {
+         // Código de estado HTTP 404, indicando que el recurso no fue encontrado.
+          status: 404,
+        }
+      );
+    }
+
+        //Solicitud procesada con éxito pero no hay contenido para retornar.
+    return new Response(null, {
+      status: 204,
+    });
+    
+    // Capturar error durante la operación
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        // Mensaje de error capturado.
+        message: error.message,
+      }),
+      // Código de estado HTTP 500, indicando un error interno del servidor.
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request, { params }) {
+  const {name, direccion, numPhone } = await request.json(); 
+
+  try {
+    const result = await Bodega.findByIdAndUpdate(params._id, {
+      name, 
+      direccion, 
+      numPhone,
+    });
 
     if (!result) {
       return new Response(
         JSON.stringify({
-          message: "Bodega no encontrado",
+          message: 'Bodega no encontrada',
         }),
         {
           status: 404,
--- src/app/api/bodega/route.js
+++ src/app/api/bodega/route.js
@@ -1,43 +1,62 @@
+// Importación de módulos
 import { connectMongoDB } from "../../../lib/mongodb";
 import Bodega from "../../../models/bodega";
 import { NextResponse } from "next/server";
 
+// Manejador de solicitudes POST para registrar una nueva devolución en la base de datos.
 export async function POST(req) {
-    try {
-      const { name, direccion, numPhone  } = await req.json();
-      await connectMongoDB();
-      await Bodega.create({ name, direccion, numPhone });
-  
-      return NextResponse.json({ message: "Bodega registrado." }, { status: 201 });
-    } catch (error) {
-      return NextResponse.json(
-        { message: "A ocurrido un error al registrar el Bodega." },
-        { status: 500 }
-      );
-    }
-  }
+  try {
+   // Extracción de los campos requeridos desde el cuerpo de la solicitud.
+    const { name, direccion, numPhone  } = await req.json();
 
-  export async function GET() {
-    try {
-      const results = await Bodega.find({});
-  
-      const mappedResults = results.map(result => ({
-        _id: result._id,
-        name: result.name,
-        direccion: result.direccion,
-        numPhone: result.numPhone,  
-      }));
-  
-      return NextResponse.json(mappedResults);
-    } catch (error) {
-      console.log('Error: ', error);
-      return NextResponse.json(
-        {
-          message: error.message,
-        },
-        {
-          status: 500,
-        }
-      );
-    }
+      // Conexión a la base de datos MongoDB.
+    await connectMongoDB();
+
+      // Creación de un nuevo documento de devolución con los campos proporcionados.
+    await Bodega.create({name, direccion, numPhone  });
+
+
+      // Devolver una respuesta JSON indicando el éxito en el registro.
+    return NextResponse.json({ message: "Bodega registrado." }, { status: 201 });
+  } catch (error) {
+      // Devolver una respuesta JSON indicando un error durante el registro.
+
+    return NextResponse.json(
+      { message: "Ha ocurrido un error al registrar el bodega." },
+      { status: 500 }
+    );
   }
+  
+}
+
+
+// Manejador de solicitudes GET para obtener todas las devoluciones desde la base de datos.
+export async function GET() {
+  try {
+      // Obtener todas las devoluciones desde la base de datos.
+    const results = await Bodega.find({});
+
+
+      // Mapear las devoluciones obtenidas a un formato más sencillo.
+    const mappedResults = results.map(result => ({
+      _id: result._id,
+      name: result.name,
+      direccion: result.direccion,
+      numPhone: result.numPhone,  
+    }));
+
+      // Devolver una respuesta JSON con las devoluciones mapeadas.
+    return NextResponse.json(mappedResults);
+  } catch (error) {
+      // Manejar cualquier error ocurrido durante el proceso y devolver una respuesta JSON indicando el error.
+    console.log('Error: ', error);
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
