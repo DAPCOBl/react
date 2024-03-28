b2d08290a27a199adfbc76d28f461fa8c6c3264f
DAPCOBl <granadosxdapcox@gmail.com>
2024-03-27 23:14:16 -0500

    error back de email

--- src/app/api/user/[_id]/route.js
+++ src/app/api/user/[_id]/route.js
@@ -2,37 +2,40 @@ import User from "../../../../models/user";
 
 export async function GET(request, { params }) {
   try {
-     const result = await User.findById(params._id);
- 
-     if (!result) {
-       return new Response(
-         JSON.stringify({
-           message: "Usuario no encontrado",
-         }),
-         {
-           status: 404,
-         }
-       );
-     }
- 
-     return new Response(JSON.stringify({
-        _id: result._id,
-        name: result.name,
-        numPhone: result.numPhone,
-        email: result.email,
-        descripcionRol: result.rol.descripcionRol,
-     }), {
-       status: 200,
-     });
+      const result = await User.findOne({ email: params });
+
+      if (!result) {
+          return new Response(
+              JSON.stringify({
+                  message: "User no encontrado",
+              }),
+              {
+                  status: 404,
+              }
+          );
+      }
+
+      return new Response(JSON.stringify({
+          _id: result._id,
+          name: result.name,
+          surname: result.surname,
+          numDoc: result.numDoc,
+          numPhone: result.numPhone,
+          email: result.email,
+          descripcionRol: result.rol.descripcionRol,
+          estado: result.rol.estado,
+      }), {
+          status: 200,
+      });
   } catch (error) {
-     return new Response(
-       JSON.stringify({
-         message: error.message,
-       }),
-       { status: 500 }
-     );
+      return new Response(
+          JSON.stringify({
+              message: error.message,
+          }),
+          { status: 500 }
+      );
   }
- }
+}
 
  export async function PUT(request, { params }) {
   const { name, numPhone, email} = await request.json(); 
